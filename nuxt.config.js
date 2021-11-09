99579e6c7e707c103368ed8d084bb416a64a33c3
khyati_bardolia <khyati.bardolia@gmail.com>
2021-11-09 15:41:30 +0530

    style: add fonts configuration and global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,22 +6,21 @@ export default {
   head: {
     title: 'js_challenge_khyati-bardolia',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [ '@/assets/styles/main.scss' ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -35,7 +34,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -44,5 +43,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
 }
--- test/NuxtLogo.spec.ts
+++ test/NuxtLogo.spec.ts
@@ -4,6 +4,7 @@ import NuxtLogo from '@/components/NuxtLogo.vue'
 describe('NuxtLogo', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(NuxtLogo)
+
     expect(wrapper.vm).toBeTruthy()
   })
 })
